c61cc32695108ffa0befef7bf584c68ad068de61
honggilgim <48556879+honggilgim@users.noreply.github.com>
2021-11-15 15:06:54 +0900

    11/15

--- 1292/1292.cpp
+++ 1292/1292.cpp
@@ -2,9 +2,30 @@
 #include <cstdio>
 using namespace std;
 
+int range(int a) {
+	int count = 0;
+	for (int i = 1; i <= a; i++)
+	{
+		count += i;
+		if (count >= a)
+		{
+			count = i;
+			break;
+		}
+	}
+	return count;
+}
+
+
 int main()
 {
 	int a, b;
+	int result = 0;
 	cin >> a >> b;
-
+	for (int i = a; i <= b; i++)
+	{
+		result += range(i);
+	}
+	cout << result;
+	return 0;
 }
