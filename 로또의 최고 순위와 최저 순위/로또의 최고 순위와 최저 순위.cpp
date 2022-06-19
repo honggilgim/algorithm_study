5becaef38c4a64274548e6c6813d13e8c73080b4
honggilgim <ghdrlf0911@naver.com>
2022-06-19 17:01:26 +0900

    06.19 문제풀이

---
+++ 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer;
+    int a = 0;
+    int k = 0;
+    for (int i = 0; i < lottos.size(); i++)
+    {
+        if (lottos[i] == 0)
+            k++;
+        for (int j = 0; j < win_nums.size(); j++)
+        {
+            if (lottos[i] == win_nums[j])
+            {
+                a++;
+                win_nums.erase(win_nums.begin() + j);
+            }
+        }
+    }
+
+    int f, s;
+
+    if (a < 2)
+        s = 6;
+    else
+        s = 6 - a + 1;
+    if (k + a < 2)
+        f = 6;
+    else
+        f = 6 - (a + k) + 1;
+
+    answer.push_back(f);
+    answer.push_back(s);
+
+    return answer;
+}
