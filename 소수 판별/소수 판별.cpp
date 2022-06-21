60f0f36cc340db53ee92f76db5027ee03eee6a10
honggilgim <ghdrlf0911@naver.com>
2022-06-21 16:45:29 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12977
    프로그래머스 소수 만들기

---
+++ 소수 판별/소수 판별.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> nums) {
+    int sum = 0;
+    int prime = 1;
+    int answer = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        for (int k = i + 1; k < nums.size(); k++) {
+            for (int j = k + 1; j < nums.size(); j++) {
+                sum = nums[i] + nums[k] + nums[j];
+                //cout << sum << "   ";
+                // 소수 판별 반복문 체 사용
+                prime = 1;
+                for (int a = 2; a <= sum / 2; a++)
+                {
+                    if (sum % a == 0) {
+                        prime = 0;
+                    }
+                }
+                if (prime == 1)
+                    answer++;
+            }
+        }
+    }
+    return answer;
+}
