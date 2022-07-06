3899b63780f2a70798894bf698f0aa89fc5a3133
honggilgim <ghdrlf0911@naver.com>
2022-07-06 23:20:34 +0900

    음양 더하기 문제풀이

---
+++ 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    int k = 0;
+    for (int i = 0; i < absolutes.size(); i++)
+    {
+        if (signs[i] == true)
+            k = absolutes[i];
+        else
+            k = -1 * absolutes[i];
+        answer += k;
+    }
+    return answer;
+}
