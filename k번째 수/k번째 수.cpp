827c5e97571cc7dc0e7cc0f73a3a89f95cc458da
honggilgim <ghdrlf0911@naver.com>
2022-07-09 23:59:00 +0900

    k번째 수 문제풀이

---
+++ k번째 수/k번째 수.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for (int i = 0; i < commands.size(); i++)
+    {
+        vector<int> arr;
+        for (int k = (commands[i][0] - 1); k < commands[i][1]; k++)
+        {
+            arr.push_back(array[k]);
+        }
+        sort(arr.begin(), arr.end());
+        answer.push_back(arr.at(commands[i][2] - 1));
+    }
+    return answer;
+}
