f164ad3e84edca744d579124358250b613607de2
honggilgim <ghdrlf0911@naver.com>
2022-07-22 23:20:00 +0900

    bfs 추가

--- 바이러스/바이러스.cpp
+++ 바이러스/바이러스.cpp
@@ -10,13 +10,33 @@ bool visited[MAX];
 
 void dfs(int a) {
 	visited[a] = true;
-	cot++;
 	for (int i = 0; i <= N; i++)
 	{
 		if (map[a][i] == 1 && visited[i] == false)
 		{
 			//cout << a << i << "\n";
+			cot++;
 			dfs(i);
+		}
+	}
+}
+
+void bfs(int a) {
+	visited[a] = true;
+	queue <int> q;
+	q.push(a);
+	while (!q.empty())
+	{
+		int k = q.front();
+		q.pop();
+		for (int i = 0; i <= N; i++)
+		{
+			if (map[k][i] == 1 && visited[i] == false)
+			{
+				q.push(i);
+				visited[i] = true;
+				cot++;
+			}
 		}
 	}
 }
@@ -33,9 +53,9 @@ int main()
 		map[a][b] = 1;
 		map[b][a] = 1;
 	}
-	dfs(1);
+	bfs(1);
 
-	cout << (cot-1);
+	cout << cot;
 
 	return 0;
 
