3953ee50be5df283096d8e5296b62b87a6515622
honggilgim <ghdrlf0911@naver.com>
2022-07-27 22:56:57 +0900

    1차

---
+++ 숨바꼭질/숨바꼭질.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <queue>
+#include <algorithm>
+#define MAX 100001
+
+using namespace std;
+
+int N, K;
+bool visited[MAX];
+int minv = MAX;
+queue<int> q;
+int cot;
+int maxv;
+
+
+
+void bfs(int a) {
+    maxv = max(N, K);
+    q.push(a);
+    visited[a] = true;
+    while (!q.empty())
+    {
+        a = q.front();
+        q.pop();
+        if (a >= 0 && a <= maxv)
+        {
+            if (a == K)
+            {
+                if (cot < minv)
+                    minv = cot;
+            }
+            if (visited[(a + 1)] == false)
+            {
+                q.push(a + 1);
+                visited[(a + 1)] = true;
+                cot++;
+            }
+            if (visited[(a - 1)] == false)
+            {
+                q.push(a - 1);
+                visited[(a - 1)] = true;
+                cot++;
+            }
+            if (visited[(2 * a)] == false)
+            {
+                q.push((2 * a));
+                visited[(a - 2)] = true;
+                cot++;
+            }
+        }
+    }
+
+}
+
+
+int main()
+{
+
+    cin >> N >> K;
+    bfs(N);
+
+    cout << minv;
+
+}
