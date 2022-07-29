eff3fc9cb73907d09aacd8c5bfcfa28ded997700
honggilgim <ghdrlf0911@naver.com>
2022-07-29 20:38:09 +0900

    연결 요소의 개수 문제풀이

---
+++ 연결 요소의 개수/연결 요소의 개수.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <queue>
+#define MAX 1001
+using namespace std;
+
+int arr[MAX][MAX];
+bool visited[MAX];
+int N, M;
+queue<int> q;
+void bfs(int a) {
+	q.push(a);
+	visited[a] = true;
+	while (!q.empty())
+	{
+		int a = q.front();
+		q.pop();
+
+		for (int i = 1; i <= N; i++)
+		{
+			if (arr[a][i] == 1 && arr[i][a] == 1 && visited[i] == false)
+			{
+				q.push(i);
+				visited[i] = true;
+			}
+		}
+	}
+}
+
+int main() {
+	int cot = 0;
+	cin >> N >> M;
+
+	int a, b;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> a >> b;
+		arr[a][b] = 1;
+		arr[b][a] = 1;
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		if (visited[i] == false)
+		{
+			bfs(i);
+			cot++;
+		}
+	}
+
+	cout << cot;
+
+	return 0;
+}
