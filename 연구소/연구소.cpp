c07b4cd36e99e3f6a7d86e34184e2915b44c2773
honggilgim <ghdrlf0911@naver.com>
2022-07-31 23:16:17 +0900

    문제풀이 1차 : 조합이 잘못됬고 초기화가 잘못되어서 그런지, bfs가 진행되지 않고 너무 많은 수를 검사함

--- 연구소/연구소.cpp
+++ 연구소/연구소.cpp
@@ -1,49 +1,144 @@
 ﻿#include <iostream>
 #include <queue>
+#include <algorithm>
 #define MAX 10
 
 int map[MAX][MAX];
 using namespace std;
+int map2[MAX][MAX];
+int map3[MAX][MAX];
+bool visited[MAX][MAX];
+int ans;
 
 int N, M;
 int dx[] = { 0, 1, 0, -1 };
 int dy[] = { 1, 0, -1, 0 };
-bool visited[MAX][MAX];
 queue<pair<int, int>> q;
+int test;
 
 void bfs() {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			map3[i][j] = map2[i][j];
+		}
+	}
+	cout << "bfs 전 : \n";
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (map3[i][j] == 2)
+				q.push(make_pair(i, j));
+			cout << map3[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
 	while (!q.empty())
 	{
 		int a = q.front().first;
 		int b = q.front().second;
+		q.pop();
 		visited[a][b] = true;
 
-		for (int i = 0; i <= 4; i++)
+		for (int i = 0; i <= 3; i++)
 		{
 			int a1 = a + dx[i];
 			int b1 = b + dy[i];
 
-			if (map[a1][b1] == 0 && visited[a1][b1] == false)
+			if (a1 > -1 && a1 < N && b1 > -1 && b1 < M && map3[a1][b1] == 0 && visited[a1][b1] == false)
 			{
-				q.push(make_pair(a1, b1));
 				visited[a1][b1] = true;
+				map3[a1][b1] = 2;
+				q.push(make_pair(a1, b1));
 			}
 		}
 	}
+	cout << "bfs 결과 : \n";
+	int cnt = 0;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (map3[i][j] == 0)
+				cnt++;
+			cout << map3[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "cnt 결과 : " << cnt << "\n";
+	ans = max(ans, cnt);
+	test++;
+}
+
+void copy() {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			map2[i][j] = map[i][j];
+			visited[i][j] = false;
+		}
+	}
+}
+
+void wall(int a)
+{
+	if (a == 3)
+	{
+		bfs();
+		return;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (map2[i][j] == 0)
+			{
+				map2[i][j] = 1;
+				wall(a + 1);
+				map2[i][j] = 0;
+			}
+		}
+	}
+
 }
 
 int main() {
 	cin >> N >> M;
-	for (int i = 0; i < M; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < M; j++)
 		{
 			cin >> map[i][j];
-			if (map[i][j] == 2)
-				q.push(make_pair(i, j));
 		}
 	}
 
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (map[i][j] == 0) {
+				copy();
+				map2[i][j] = 1;
+				wall(1);
+				map2[i][j] = 0;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << map[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+
+	cout << ans;
+
+	cout << "bfs 수행 횟수 :" << test;
+
+	return 0;
 
 }
-
