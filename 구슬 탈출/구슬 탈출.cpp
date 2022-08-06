74303fd6a92e1c3ef82d08c11bbbbcca0f8d6b04
honggilgim <ghdrlf0911@naver.com>
2022-08-06 23:56:48 +0900

    오류수정중

--- 구슬 탈출/구슬 탈출.cpp
+++ 구슬 탈출/구슬 탈출.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 int n, m;
 string map[MAX][MAX];
-bool visited[MAX][MAX];
+bool visited[MAX][MAX][MAX][MAX];
 
 struct ballindex
 {
@@ -21,147 +21,74 @@ struct ballindex
 
 queue <ballindex> q;
 
-int ans_x, ans_y;
+int dx[] = { 1,0,-1,0 };
+int dy[] = { 0,1,0,-1 };
 
-int cot;
-int bfs(int rx, int ry, int bx, int by)
+int ball(int& x, int& y, int i)
 {
-	q.push({ rx, ry, bx, by });
-	visited[rx][ry] = true;
-	cout << "bfs 시작 \n";
+	int count = 1;
+	while ((map[x + dx[i]][y + dy[i]] != "#") && (map[x][y] != "O"))
+	{
+		x += dx[i];
+		y += dy[i];
+		count++;
+	}
+	return count;
+}
+
+int bfs()
+{
+	int cot = -1;
 	while (!q.empty())
 	{
-		cout << "1\n";
 		if (cot > 10)
 			return -1;
 		int rx = q.front().rx;
 		int ry = q.front().ry;
 		int bx = q.front().bx;
 		int by = q.front().by;
-		q.pop();
-		cout << "2\n";
-		cout << " rx : " << rx << " ry :" << ry << " bx : " << bx << " by : " << by << " cot :" << cot << "\n";
-		if (rx == ans_x && ry == ans_y)
-		{
-			cout << "성공!";
-			return cot;
-		}
-		int rx_next = rx;
-		int bx_next = bx;
-		int ry_next = ry;
-		int by_next = by;
-		bool break_point = false;
-		cout << "map[rx_next][ry_next] : " << map[rx_next][ry_next] << " map[rx_next][ry_next] : " << map[rx_next][ry_next] << "3\n" ;
-		// 오른쪽
-		while (map[rx_next][ry_next] != "#" && map[rx_next][ry_next] != "0")
-		{
-			rx_next++;
-		}
-		cout << "4\n";
-		while (map[bx_next][by_next] == ".")
-		{
-			if (map[bx_next][by_next] == "O")
-			{
-				break_point = true;
-				break;
-			}
-			bx_next++;
-		}
-		cout << "5" << " R " << rx_next << " b " << bx_next << "\n";
-		if (visited[rx_next][ry_next] == false && !break_point)
-		{
-			cout << "오른쪽 rx : " << rx_next << " ry :" << ry_next << " bx : " << bx_next << " by : " << by_next << "\n";
-			visited[rx_next][ry_next] = true;
-			q.push({ rx_next,ry_next,bx_next,by_next });
-		}
-
-		cout << "6\n";
-		rx_next = rx;
-		bx_next = bx;
-		ry_next = ry;
-		by_next = by;
-		break_point = false;
-		//왼쪽
-		while (map[rx_next][ry_next] == "." && map[rx_next][ry_next] != "O")
-		{
-			rx_next--;
-		}
-		while (map[bx_next][by_next] == ".")
-		{
-			if (map[bx_next][by_next] == "O")
-			{
-				break_point = true;
-				break;
-			}
-			bx_next--;
-		}
-		cout << "7\n";
-		if (visited[rx_next][ry_next] == false && !break_point)
-		{
-			cout << "왼쪽 rx : " << rx_next << " ry :" << ry_next << " bx : " << bx_next << " by : " << by_next << "\n";
-			visited[rx_next][ry_next] = true;
-			q.push({ rx_next,ry_next,bx_next,by_next });
-		}
-		cout << "8\n";
-		rx_next = rx;
-		bx_next = bx;
-		ry_next = ry;
-		by_next = by;
-		break_point = false;
-
-		// 위로
-		while (map[rx_next][ry_next] == "." && map[rx_next][ry_next] != "O")
-		{
-			ry_next++;
-		}
-		cout << "9\n";
-		while (map[bx_next][by_next] == ".")
-		{
-			if (map[bx_next][by_next] == "O")
-			{
-				break_point = true;
-				break;
-			}
-			by_next++;
-		}
-		cout << "10\n";
-		if (visited[rx_next][ry_next] == false && !break_point)
-		{
-			cout << "위쪽 rx : " << rx_next << " ry :" << ry_next << " bx : " << bx_next << " by : " << by_next << "\n";
-			visited[rx_next][ry_next] = true;
-			q.push({ rx_next,ry_next,bx_next,by_next });
-		}
-
-		rx_next = rx;
-		bx_next = bx;
-		ry_next = ry;
-		by_next = by;
-		break_point = false;
-		cout << "11\n";
-		//아래로
-		while (map[rx_next][ry_next] == "." && map[rx_next][ry_next] != "O")
-		{
-			ry_next--;
-		}
-		while (map[bx_next][by_next] == ".")
-		{
-			if (map[bx_next][by_next] == "O")
-			{
-				break_point = true;
-				break;
-			}
-			by_next--;
-		}
-		cout << "12\n";
-		if (visited[rx_next][ry_next] == false && !break_point)
-		{
-			cout << "아래쪽 rx : " << rx_next << " ry :" << ry_next << " bx : " << bx_next << " by : " << by_next << "\n";
-			visited[rx_next][ry_next] = true;
-			q.push({ rx_next,ry_next,bx_next,by_next });
-		}
-		cout << "13\n";
 		cot++;
+		cout << " rx : " << rx << " ry :" << ry << " bx : " << bx << " by : " << by << "cot : " << cot << "\n";
+		q.pop();
 
+		for (int i = 0; i < 4; i++)
+		{
+			int rrx = rx;
+			int rry = ry;
+			int bbx = bx;
+			int bby = by;
+			int redmove = 0;
+			int bluemove = 0;
+
+			redmove = ball(rrx, rry,i);
+			cout << "rrx : " << rrx << "rry : " << rry << "i :  " << i << "\n";
+			bluemove = ball(bbx, bby, i);
+			cout << "bbx : " << bbx << "bby : " << bby << "i :  " << i << "\n\n";
+			if (map[bbx][bby] == "O") continue;
+			if (map[rrx][rry] == "O")
+			{
+				cout << rrx << rry << "\n";
+				cout << "성공!";
+				return cot;
+			}
+			if (rrx == bbx && rry == bby)
+			{
+				if (redmove > bluemove)
+				{
+					rrx -= dx[i];
+					rry -= dy[i];
+				}
+				else
+				{
+					bbx -= dx[i];
+					bby -= dy[i];
+				}
+			}
+			if (visited[rrx][rry][bbx][bby]) continue;
+			visited[rrx][rry][bbx][bby] = true;
+			
+			q.push({ rrx,rry,bbx,bby });
+		}
 	}
 }
 
@@ -170,11 +97,11 @@ int main() {
 	cin >> n >> m;
 	int bx, by;
 	int rx, ry;
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < n; i++)
 	{
 		string s;
 		cin >> s;
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < m; j++)
 		{
 			map[i][j] = s[j];
 			
@@ -188,26 +115,22 @@ int main() {
 				bx = j;
 				by = i;
 			}
-			if (map[i][j] == "O")
-			{
-				ans_x = j;
-				ans_y = i;
-			}
 		}
 	}
 
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < m; j++)
 		{
 			cout << map[i][j] << " ";
 		}
 		cout << "\n";
 	}
 
-	cout << " rx : " << rx << " ry :" << ry << " bx : " << bx << " by : " << by <<" ans_x : "<< ans_x << " ans_y " << ans_y << "\n";
+	//cout << " rx : " << rx << " ry :" << ry << " bx : " << bx << " by : " << by <<"\n";
 
-	cout << bfs(rx, ry, bx, by);
+	q.push({ rx, ry, bx, by });
+	cout << bfs();
 
 	return 0;
 
