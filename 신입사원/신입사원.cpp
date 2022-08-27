6fd4aaf3e07756b9e63c07fe8f3b4df3a1b6c507
honggilgim <ghdrlf0911@naver.com>
2022-08-27 23:22:40 +0900

    문제풀이

---
+++ 신입사원/신입사원.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	vector<int> an;
+	for (int i = 0; i < T; i++)
+	{
+		int N;
+		vector<pair<int, int>> a;
+		cin >> N;
+		for (int k = 0; k < N; k++)
+		{
+			int b, c;
+			cin >> b >> c;
+			a.push_back({ b,c });
+		}
+		sort(a.begin(), a.end());
+		int ans = 0;
+		int max = a[0].second;
+		for (int i = 0; i < N; i++)
+		{
+			if (a[i].second <= max)
+			{
+				ans++;
+				max = a[i].second;
+			}
+		}
+		an.push_back(ans);
+
+	}
+	for (int i = 0; i < T; i++)
+	{
+		cout << an[i] << endl;
+	}
+	return 0;
+}
