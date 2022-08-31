a8c136fb7b826ec7953ff4a7e779b86443aaaae0
honggilgim <ghdrlf0911@naver.com>
2022-08-31 23:54:16 +0900

    문제풀이 완료

---
+++ 카드 정렬하기/카드 정렬하기.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+/*
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int> card;
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+		card.push_back(x);
+	}
+	int answer = 0;
+	while (card.size() > 1)
+	{
+		sort(card.begin(), card.end());
+		answer += card[0] + card[1];
+		card[1] = card[0] + card[1];
+		card.erase(card.begin());
+		//cout << answer << endl;
+	}
+	cout << answer;
+	return 0;
+}
+*/
+
+int main() {
+	priority_queue<int, vector<int>, greater<int>> q;
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+		q.push(x);
+	}
+	int answer = 0;
+	while (q.size() > 1)
+	{
+		int a, b;
+		a = q.top();
+		q.pop();
+		b = q.top();
+		q.pop();
+		answer += (a + b);
+		q.push(a + b);
+	}
+	cout << answer;
+	return 0;
+}
