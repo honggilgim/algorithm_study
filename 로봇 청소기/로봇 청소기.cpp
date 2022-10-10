b7b727cc3c8006951e2839310a1ea333755023a7
honggilgim <ghdrlf0911@naver.com>
2022-10-10 23:08:22 +0900

    new

--- 로봇 청소기/로봇 청소기.cpp
+++ 로봇 청소기/로봇 청소기.cpp
@@ -3,100 +3,49 @@
 
 using namespace std;
 int N, M;
-int arr[MAX][MAX] = {1,};
-bool visited[MAX][MAX];
-int ans;
+int arr[MAX][MAX];
+int dx[] = { -1,0,1,0 };
+int dy[] = { 0,1,0,-1 };
 
 void visit(int x, int y,int d, int count)
 {
-	if ( x > 0 && d == 0 && arr[x - 1][y] == 0 && visited[x-1][y] == false)
-	{
-		visited[x - 1][y] = true;
-		visit(x - 1, y, 3, count + 1);
+	if (count >= 4) {
+		int nx = x - dx[d];
+		int ny = y - dy[d];
+		if (nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] == 2)
+			visit(nx, ny, d, 0);
 		return;
 	}
-	if (d == 0 && (arr[x - 1][y] == 1 || visited[x - 1][y] == true))
+	arr[x][y] = 2;
+	int nd = ((d - 1) + 4) % 4;
+	int nx = x + dx[nd];
+	int ny = y + dy[nd];
+	if (nx >= 0 && nx < N && ny >= 0 && ny < M)
 	{
-		visit(x, y, 3, count);
-		return;
+		if (arr[nx][ny] == 0)
+			visit(nx, ny, nd, 0);
+		else if (arr[nx][ny] == 1 || arr[nx][ny] == 2)
+			visit(x, y, nd, count + 1);
 	}
-	if (d == 1 && arr[x][y - 1] == 0 && visited[x][y - 1] == false)
-	{
-		visited[x][y-1] = true;
-		visit(x, y - 1, 0, count + 1);
-		return;
-	}
-	if (d == 1 && (arr[x][y-1] == 1 || visited[x][y-1] == true))
-	{
-		visit(x, y, 0, count);
-		return;
-	}
-	if (d == 2 && arr[x + 1][y] == 0 && visited[x + 1][y] == false)
-	{
-		visited[x + 1][y] = true;
-		visit(x + 1, y, 1, count + 1);
-		return;
-	}
-	if (d == 2 && (arr[x + 1][y] == 1 || visited[x + 1][y] == true))
-	{
-		visit(x, y, 1, count);
-		return;
-	}
-	if (d == 3 && arr[x][y + 1] == 0 && visited[x][y + 1] == false)
-	{
-		visited[x][y + 1] == true;
-		visit(x, y + 1, 2, count + 1);
-		return;
-	}
-	if (d == 3 && (arr[x][y + 1] == 1 || visited[x][y + 1] == true))
-	{
-		visit(x, y, 2, count);
-		return;
-	}
-	else
-	{
-		if (d == 0 && arr[x][y + 1] == 0)
-		{
-			visit(x, y + 1, 0, count);
-			return;
-		}
-		if (d == 1 && arr[x - 1][y] == 0)
-		{
-			visit(x - 1, y, 1, count);
-			return;
-		}
-		if (d == 2 && arr[x][y - 1] == 0)
-		{
-			visit(x, y - 1, 0, count);
-			return;
-		}
-		if (d == 3 && arr[x + 1][y] == 0)
-		{
-			visit(x + 1, y, 3, count);
-			return;
-		}
-		else
-		{
-			ans = count;
-			return;
-		}
-	}
+
 }
 
 int main() {
+	int ans = 0;
 	int x, y;
 	int d; // d : 방향
 	//d 0 북쪽 1 동쪽 2 남쪽 3 서쪽
 	cin >> N >> M;
 	cin >> x >> y >> d;
 	// N 세로 M 가로
-	for (int i = 0; i < M; i++)
-		for (int j = 0; j < N; j++)
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
 			cin >> arr[i][j];
-	visited[x][y] == true;
-	visit(x, y, d, 1);
-	cout << ans;
+	visit(x, y, d, 0);
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			if (arr[i][j] == 2)
+				ans++;
+	cout << ans << "\n";
 	return 0;
-
-
 }
