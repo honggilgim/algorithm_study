e0d50204d738a3f5c4d5a883093e844e5b959da3
honggilgim <ghdrlf0911@naver.com>
2022-10-24 23:48:06 +0900

    회의실 배정 문제풀이 완료

---
+++ 그리디 알고리즘/강의실 배정/아기상어/강의실 배정.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	vector <pair<int,int>> time;
+	priority_queue<int, vector<int>, greater<int>> pq; // 사용하는 클래스의 개수
+	int N;
+	cin >> N;
+	int start, end;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> start >> end;
+		time.push_back({ start,end });
+	}
+
+	sort(time.begin(),time.end());
+	pq.push(time[0].second); // 교실 하나에서 수업 시작
+
+	int count=1;
+	for (int i = 1; i < N; i++)
+	{
+		if (pq.top() <= time[i].first) // 시작 시간보다 끝나는 시간이 작으면 교실 수업 변경
+		{
+			pq.pop();
+			pq.push(time[i].second);
+		}
+		else { // 교실 추가
+			pq.push(time[i].second);
+		}
+	}
+	cout << pq.size();
+	return 0;
+	
+}
