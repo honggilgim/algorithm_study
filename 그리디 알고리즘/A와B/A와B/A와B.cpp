319953ba7109a86c516e08c32fd84d732b1a1178
honggilgim <ghdrlf0911@naver.com>
2022-10-31 22:10:56 +0900

    A와B

---
+++ 그리디 알고리즘/A와B/A와B/A와B.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	string S;
+	string T;
+	int answer = 0;
+
+	//문자열 뒤에 A 추가
+	// 문자열 뒤집고 B 추가
+
+	cin >> S;
+	cin >> T;
+
+	while (true)
+	{
+		if (S.size() == T.size())
+		{
+			if (S == T)
+				answer = 1;
+			break;
+		}
+		//cout << T[T.size()-1] << "\n";
+
+		if (T[T.size()-1] == 'A')
+			T.pop_back();
+		else
+		{
+			T.pop_back();
+			reverse(T.begin(), T.end());
+		}
+	}
+	cout << answer;
+	return 0;
+
+}
