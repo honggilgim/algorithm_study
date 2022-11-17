33e6c12e98d9eef93425c10259bf823e53acb814
honggilgim <ghdrlf0911@naver.com>
2022-11-17 21:52:55 +0900

    hash

---
+++ 해시/firsthash/firsthash/firsthash.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    unordered_map<string, int> map;
+    for (auto man : participant)
+    {
+        if (map.end() == map.find(man))
+            map.insert(make_pair(man, 1));
+        else
+            map[man]++;
+    }
+    for (auto man : completion)
+        map[man]--;
+
+    for (auto man : participant)
+    {
+        if (map[man] > 0)
+        {
+            answer = man;
+            break;
+        }
+    }
+    return answer;
+}
