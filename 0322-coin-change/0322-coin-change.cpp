399f7181a82c20a065797d1c1f63f98f2a554727
honggilgim <48556879+honggilgim@users.noreply.github.com>
2024-12-22 20:10:44 +0900

    Time: 15 ms (92.72%), Space: 13.2 MB (93.64%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int coinChange(vector<int>& coins, int amount) {
+        int dp[10001];
+        sort(coins.begin(),coins.end());
+        for(int i=1; i<=amount; i++) {
+            dp[i] = 2147483640;
+            for(int j=0; j<coins.size();j++) {
+                if((i-coins[j]) < 0 || i-coins[j] > 10001) continue;
+                dp[i] = min(dp[i],(dp[i-coins[j]]+1));
+            }
+        }
+
+        return dp[amount] == 2147483640 ? -1 : dp[amount];
+
+
+    }
+};
