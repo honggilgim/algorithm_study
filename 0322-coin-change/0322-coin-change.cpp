23240237973844bb404c003b9383799cf2a600ec
honggilgim <48556879+honggilgim@users.noreply.github.com>
2024-12-22 20:24:49 +0900

    Time: 17 ms (85.97%), Space: 13.3 MB (93.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,12 +5,12 @@ public:
         int dp[10001];
         sort(coins.begin(),coins.end());
         for(int i=1; i<=amount; i++) {
-            dp[i] = 2147483640;
+            dp[i] = 10001;
             for(int temp : coins) {
                 if((i-temp) < 0) continue;
                 dp[i] = min(dp[i],(dp[i-temp]+1));
             }
         }
-        return dp[amount] == 2147483640 ? -1 : dp[amount];
+        return dp[amount] == 10001 ? -1 : dp[amount];
     }
 };
